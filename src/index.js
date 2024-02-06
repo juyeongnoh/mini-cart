ed58921209338a6113b4e0ed9a320b9e8112d78d
juyeongnoh <juyeongnoh@gmail.com>
2024-02-06 20:58:34 +0900

    feat: 비동기 API 요청 모킹, 상품 목록 렌더링

--- src/index.js
+++ src/index.js
@@ -1,1 +1,52 @@
 // 이 곳에 정답 코드를 작성해주세요.
+
+const $productList = document.getElementById('product-list');
+const $productCardGrid = document.getElementById('product-card-grid');
+
+// 1. 비동기 API 요청 모킹하기
+async function getItem() {
+  const response = await fetch('/src/api/productData.json')
+    .then((res) => res.json())
+    .then((json) => {
+      json.forEach((item) => {
+        $productCardGrid.appendChild(
+          generateProductCard(item.id, item.imgSrc, item.name, item.price)
+        );
+      });
+    });
+}
+
+// 2. 상품 목록 렌더링하기
+const generateProductCard = (id, imgSrc, name, price) => {
+  const $productCard = document.createElement('article');
+  const $wrapper = document.createElement('div');
+  const $img = document.createElement('img');
+  const $hover = document.createElement('div');
+  const $id = document.createElement('div');
+  const $name = document.createElement('h3');
+  const $price = document.createElement('p');
+
+  $wrapper.className = 'rounded-lg overflow-hidden border-2 relative';
+  $img.className = 'object-center object-cover';
+  $hover.className =
+    'hover:bg-sky-500 w-full h-full absolute top-0 left-0 opacity-90 transition-colors ease-linear duration-75';
+  $id.className =
+    'hover:opacity-100 opacity-0 w-full h-full flex justify-center items-center text-xl text-white font-bold cursor-pointer';
+  $name.className = 'mt-4 text-gray-700';
+  $price.className = 'mt-1 text-lg font-semibold text-gray-900';
+
+  $productCard.setAttribute('id', 'product-card');
+  $id.setAttribute('data-productid', id);
+  $id.innerText = '장바구니에 담기';
+  $img.src = imgSrc;
+  $name.innerText = name;
+  $price.innerText = price.toLocaleString() + '원';
+
+  $hover.append($id);
+  $wrapper.append($img, $hover);
+  $productCard.append($wrapper, $name, $price);
+
+  return $productCard;
+};
+
+getItem();
