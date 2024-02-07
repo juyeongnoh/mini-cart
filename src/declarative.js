afcb56911f54828df877d307b9d7f5b5d5742070
juyeongnoh <juyeongnoh@gmail.com>
2024-02-07 23:45:32 +0900

    feat: 상품 목록 렌더링

--- src/declarative.js
+++ src/declarative.js
@@ -1,8 +1,12 @@
 import getProductData from './api/getProductData.js';
+import ProductList from './components/ProductList.js';
+
+const $productCardGrid = document.getElementById('product-card-grid');
+const productList = new ProductList($productCardGrid, []);
 
 const fetchProductData = async () => {
   const result = await getProductData();
-  console.log(result);
+  productList.setState(result);
 };
 
 fetchProductData();
