0cf370308b3994caa8c9f7d1cc2bfdd47e369b12
juyeongnoh <juyeongnoh@gmail.com>
2024-02-07 23:55:41 +0900

    feat: 장바구니 토글 기능

--- src/declarative.js
+++ src/declarative.js
@@ -2,7 +2,18 @@ import getProductData from './api/getProductData.js';
 import ProductList from './components/ProductList.js';
 
 const $productCardGrid = document.getElementById('product-card-grid');
+const $openCartBtn = document.getElementById('open-cart-btn');
+const $closeCartBtn = document.getElementById('close-cart-btn');
+const $shoppingCart = document.getElementById('shopping-cart');
+const $backdrop = document.getElementById('backdrop');
+
 const productList = new ProductList($productCardGrid, []);
+
+const toggleCart = () => {
+  $shoppingCart.classList.toggle('translate-x-0');
+  $shoppingCart.classList.toggle('translate-x-full');
+  $backdrop.hidden = !$backdrop.hidden;
+};
 
 const fetchProductData = async () => {
   const result = await getProductData();
@@ -10,3 +21,8 @@ const fetchProductData = async () => {
 };
 
 fetchProductData();
+
+$openCartBtn.addEventListener('click', toggleCart);
+$closeCartBtn.addEventListener('click', toggleCart);
+$backdrop.addEventListener('click', toggleCart);
+$productCardGrid.addEventListener('click', toggleCart);
